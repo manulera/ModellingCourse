e57dfcd4eaf9d8cff6e0e12a011fc5d3dd7969a8
manulera <manulera14@gmail.com>
2016-11-03 20:35:00 +0100

    Working on predator-pray model

---
+++ ReAct/PredatorPray.py
@@ -0,0 +1,45 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import random
+
+k=[1,0.11,0.01,1]
+init=np.array([80,40])
+dt=0.001
+t=np.arange(0,10,dt)
+
+def myODE(init,t,k):
+    x,y=init
+    A,B,C,D=k
+    dx=A*x-B*x*y
+    dy=C*x*y-D*y
+    return dx,dy
+
+detsolution = odeint(myODE, init,t,args=(k,))
+print 'Deterministic finished'
+
+def StoSol(init,t,k):
+
+    stosol=np.zeros([t.shape[0],2])
+    dt = t[1] - t[0]
+    for i in range(t.size):
+        Ps = myODE(init,t,k)
+
+        booly=[Ps[0]*dt>random.random(),Ps[1]*dt>random.random()]
+        init = init + np.array(booly)
+        stosol[i,:]=init
+    return stosol
+
+stosolution=StoSol(init,t,k)
+
+
+plt.figure()
+
+plt.plot(t,detsolution[:,0],label='prey')
+plt.plot(t,detsolution[:,1],label='predator')
+plt.step(t,stosolution[:,0],label='prey')
+plt.step(t,stosolution[:,1],label='predator')
+
+print stosolution
+plt.legend()
+plt.show()
--- ReAct/test.py
+++ ReAct/test.py
@@ -1,18 +1,2 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-X = np.arange(1, 10)
-Y = np.arange(1, 10)
-X, Y = np.meshgrid(X, Y)
-print X.shape
-
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='hot', linewidth=0, antialiased=False)
-ax.set_zlim(-1.01, 1.01)
-
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.show()
+a=(2,3)
+print a*2
